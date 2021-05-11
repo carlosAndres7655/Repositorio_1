d322a12231ff7de2359f6bae05965a70226f0e50
carlosAndres7655 <79858619+carlosAndres7655@users.noreply.github.com>
2021-05-11 13:37:53 -0400

    Add files via upload

---
+++ register.php
@@ -0,0 +1,170 @@
+<?php
+
+include "db.php";
+
+$f_name = $_POST["f_name"];
+$l_name = $_POST["l_name"];
+$email = $_POST['email'];
+$password = $_POST['password'];
+$repassword = $_POST['repassword'];
+$mobile = $_POST['mobile'];
+$address1 = $_POST['address1'];
+$address2 = $_POST['address2'];
+$name = "/^[A-Z][a-zA-Z ]+$/";
+$emailValidation = "/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9]+(\.[a-z]{2,4})$/";
+$number = "/^[0-9]+$/";
+
+if(empty($f_name) || empty($l_name) || empty($email) || empty($password) || empty($repassword) ||
+	empty($mobile) || empty($address1) || empty($address2)){
+		
+		echo "
+			<div class='alert alert-warning'>
+				<a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a><b>Por favor llena todos los espacios
+..!</b>
+			</div>
+		";
+		exit();
+	} else {
+
+	if(!preg_match($emailValidation,$email)){
+		echo "
+			<div class='alert alert-warning'>
+				<a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>
+				<b>esta $email no es válido..!</b>
+			</div>
+		";
+		exit();
+	}
+	if(strlen($password) < 9 ){
+		echo "
+			<div class='alert alert-warning'>
+				<a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>
+				<b>La contraseña es débil</b>
+			</div>
+		";
+		exit();
+	}
+	if(strlen($repassword) < 9 ){
+		echo "
+			<div class='alert alert-warning'>
+				<a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>
+				<b>La contraseña es débil</b>
+			</div>
+		";
+		exit();
+	}
+	if($password != $repassword){
+		echo "
+			<div class='alert alert-warning'>
+				<a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>
+				<b>la contraseña no es lo mismo</b>
+			</div>
+		";
+	}
+	if(!preg_match($number,$mobile)){
+		echo "
+			<div class='alert alert-warning'>
+				<a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>
+				<b>El número $mobile no es lo mismo</b>
+			</div>
+		";
+		exit();
+	}
+	if(!(strlen($mobile) == 10)){
+		echo "
+			<div class='alert alert-warning'>
+				<a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>
+				<b>El número de móvil debe ser de 10 dígitost</b>
+			</div>
+		";
+		exit();
+	}
+	//existing email address in our database
+	$sql = "SELECT user_id FROM user_info WHERE email = '$email' LIMIT 1" ;
+	$check_query = mysqli_query($con,$sql);
+	$count_email = mysqli_num_rows($check_query);
+	if($count_email > 0){
+		echo "
+			<div class='alert alert-danger'>
+				<a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>
+				<b>La dirección de correo electrónico ya está disponible Pruebe otra dirección de correo electrónico
+</b>
+			</div>
+		";
+		exit();
+	} else {
+		$password = md5($password);
+		$sql = "INSERT INTO `user_info` 
+		(`user_id`, `first_name`, `last_name`, `email`, 
+		`password`, `mobile`, `address1`, `address2`) 
+		VALUES (NULL, '$f_name', '$l_name', '$email', 
+		'$password', '$mobile', '$address1', '$address2')";
+		$run_query = mysqli_query($con,$sql);
+		if($run_query){
+			echo "
+				<div class='alert alert-success'>
+					<a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>
+				<b>Usted está registrado con éxito..!</b>
+				</div>
+			";
+		}
+	}
+	}
+	
+
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
